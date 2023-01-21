74ff70c77925def5c195583d0dc9d28b802c2a0e
santo28 <santos17004@byui.edu>
2023-01-21 15:00:13 -0700

    Changed 200 status

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -62,7 +62,7 @@ const deleteContact = async (req, res) => {
   const response = await mongodb.getDb().db().collection('contacts').remove({ _id: userId }, true);
   console.log(response);
   if (response.deletedCount > 0) {
-    res.status(204).send();
+    res.status(200).send();
   } else {
     res.status(500).json(response.error || 'Some error occurred while deleting the contact.');
   }
